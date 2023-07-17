b78dd10d9e90f088958c03c157786051d5de7fee
Zaiddal <88930179+Zaiddal@users.noreply.github.com>
2023-07-17 20:30:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-song=""
-leftWristx = 0
-leftWristy = 0
-rightWristx = 0
-rightWristy = 0
+song="";
+song1status= "";
+song2status="";
+scoreleftwrist = 0;
+scorerightwrist = 0;
+leftWristx = 0;
+leftWristy = 0;
+rightWristx = 0;
+rightWristy = 0;
 function setup(){
     canvas = createCanvas(600, 500);
     canvas.center();
@@ -14,6 +18,28 @@ function setup(){
 
 function draw(){
     image(video, 0, 0, 600, 500);
+    stroke("yellow");
+    fill("yellow");
+    song1status = song1.isPlaying();
+    song2status = song2.isPlaying();
+    if(scoreleftwrist > 0.2 ){
+        circle(leftWristx, leftWristy, 20);
+        song2.stop();
+        document.getElementById("song").innerHTML;
+        if(song1status == false){
+            song1.play();
+        }
+
+    }
+    if(scorerightwrist>0.2){
+        circle(rightWristx, rightWristy, 20);
+        song1.stop();
+        document.getElementById("song").innerHTML;
+        if(song2status == false){
+            song2.play();
+        }
+    }
+
 }
 
 function preload(){
@@ -34,5 +60,8 @@ function gotresults(results){
         leftWristy = results[0].pose.leftWrist.y;
         rightWristx = results[0].pose.rightWrist.x;
         rightWristy = results[0].pose.rightWrist.y;
+         scoreleftwrist = results[0].pose.keypoints[9].score;
+         scorerightwrist = results[0].pose.keypoints[10].score;
     }
+
 }
